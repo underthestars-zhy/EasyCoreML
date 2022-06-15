e6ed2067efc34cb2196dd37f64f4c5e981b703f6
Underthestars-zhy <zhuhaoyu0909@icloud.com>
2022-06-15 20:52:13 +0800

    fix

--- Sources/EasyCoreML/EasyCoreML.swift
+++ Sources/EasyCoreML/EasyCoreML.swift
@@ -1,6 +1,73 @@
-public struct EasyCoreML {
-    public private(set) var text = "Hello, World!"
+import CoreML
+import Vision
 
-    public init() {
+#if canImport(UIKit)
+import UIKit
+#elseif canImport(AppKit)
+import AppKit
+
+public typealias UIImage = NSImage
+
+public extension NSImage {
+    var cgImage: CGImage? {
+        return self.cgImage(forProposedRect: nil, context: nil, hints: nil)
     }
+}
+#endif
+
+public struct EasyCoreML {
+    let model: MLModel
+    
+    public init(_ model: MLModel) {
+        self.model = model
+    }
+    
+    public func test(_ cgImage: CGImage, identifier: String, min: Double = 50) async throws -> Bool {
+        guard let model = try? VNCoreMLModel(for: model) else {
+            return false
+        }
+        
+        return try await withCheckedThrowingContinuation { continuation in
+            let request = VNCoreMLRequest(model: model) {request, error in
+                let results = request.results as? [VNClassificationObservation]
+                
+                guard let res = results?.first(where: {
+                    $0.identifier == identifier
+                }) else {
+                    continuation.resume(returning: false)
+                    return
+                }
+                
+                continuation.resume(returning: Double(res.confidence) * Double(100) >= min)
+            }
+            
+            let handler = VNImageRequestHandler(cgImage: cgImage)
+            
+            DispatchQueue.global(qos: .userInteractive).async {
+                do {
+                    try handler.perform([request])
+                } catch {
+                    continuation.resume(throwing: error)
+                }
+            }
+        }
+    }
+    
+    public func test(_ data: Data, identifier: String, min: Double = 50) async throws -> Bool {
+        guard let image = UIImage(data: data)?.cgImage else {
+            throw NSError()
+        }
+        
+        return try await self.test(image, identifier: identifier, min: min)
+    }
+    
+#if canImport(UIKit) || canImport(AppKit)
+    public func test(_ image: UIImage, identifier: String, min: Double = 50) async throws -> Bool {
+        guard let image = image.cgImage else {
+            throw NSError()
+        }
+        
+        return try await self.test(image, identifier: identifier, min: min)
+    }
+#endif
 }
--- Tests/EasyCoreMLTests/EasyCoreMLTests.swift
+++ Tests/EasyCoreMLTests/EasyCoreMLTests.swift
@@ -1,11 +1,12 @@
 import XCTest
+import CoreML
 @testable import EasyCoreML
 
 final class EasyCoreMLTests: XCTestCase {
-    func testExample() throws {
+    func testExample() async throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(EasyCoreML().text, "Hello, World!")
+        
     }
 }
